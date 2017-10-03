c60defae1b1e264250bb47806f3b87ffa7a33468
Zakaria El Harti <zak_el_harti@msn.com>
2017-10-03 23:35:02 +0200

    question 7 a fin

--- ChaineCryptee.java
+++ ChaineCryptee.java
@@ -12,37 +12,19 @@ public class ChaineCryptee
     
     
     
+  
     
     /**
      * 
-     *@param Si la chaine passée en clair est null alors j'initialise a "vide".
+     *@return Si la chaine passée en clair est null alors j'initialise a "vide".
      */
     private ChaineCryptee(String enclair,int decalage)
     {
         
         if(enclair==null) {enclair="VIDE";}
-        
+      
         this.decalage=decalage;
-        this.enclair=enclair;
-        
-    }
-    
-         private static char decaleCaractere (char c, int decalage)
-    {
-        return ( c < 'A' || c > 'Z') ? c : (char)(( (c - 'A' + decalage) % 26) + 'A');
-    }
-
-    
-    
-    public String decrypte()
-    {
-        return enclair;
-    }
-    
-    
-    public String crypte()
-    {
-        
+      
         char a;
         String chiffrée="";
         int i;
@@ -51,9 +33,58 @@ public class ChaineCryptee
             a=decaleCaractere(enclair.charAt(i),decalage);
             chiffrée+=a;
         }
-        return chiffrée;
+        enclair=chiffrée;
+        
+    }
+    
+   
+        private static char decaleCaractere (char c, int decalage)
+    {
+        return ( c < 'A' || c > 'Z') ? c : (char)(( (c - 'A' + decalage) % 26) + 'A');
+    }
+    
+    
+    public String decrypte()
+    {
+        String chaine = "";
+        char a;
+         
+        int i;
+        
+                for(i=0;i<enclair.length();i++)
+           { a=decaleCaractere(enclair.charAt(i),-decalage); 
+             chaine += a;
+           }
+             
+        return chaine;
+        
+    }
+    
+    
+    public String crypte()
+    {
+        return enclair;
+        
     }
    
+     public static ChaineCryptee FromCrypted(String incrypted, int decalage) { 
+
+        int i;
+        char a;
+        String chaine_pour_decrypter = "";
+        
+        for(i=0;i<incrypted.length();i++)
+           { a=decaleCaractere(incrypted.charAt(i),-decalage ); 
+             chaine_pour_decrypter += a;
+            }
+            
+        ChaineCryptee chaine_a_crypter = new ChaineCryptee(chaine_pour_decrypter, decalage);
+        return chaine_a_crypter;
+    }
     
+        public static ChaineCryptee FromDecrypted(String indecrypted, int decalage) {
+        ChaineCryptee chaine_a_crypter = new ChaineCryptee(indecrypted,decalage);
+        return chaine_a_crypter;
+    }
    
 }
